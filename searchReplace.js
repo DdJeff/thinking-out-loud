6e482731d608645dd1b0d3d7324cd76f91c82dff
DdJeff <djefferson46@gmail.com>
2021-04-17 21:49:56 -0500

    reaplaceing words

---
+++ searchReplace.js
@@ -0,0 +1,29 @@
+function myReplace(str, before, after) {
+    // make a fucntion to change case
+    
+      const checkCase = (index) =>{
+        let upper = index[0].toUpperCase()
+        let end =  index.slice(1)
+        return upper.concat(end)
+      }
+    //breaking up word
+      let a = str.split(" ")
+    
+    // check for word
+      for(let i = 0; i < a.length; i++){
+    
+        if(a[i] == before){
+
+          if(a[i][0] !== a[i][0].toUpperCase()){
+               a[i] = after.toLowerCase();
+          }else{
+               a[i] = checkCase(after)  
+          }  
+        }
+      }
+     
+      str = (a.join(" "))
+      return str;
+    }
+    
+    myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped");
